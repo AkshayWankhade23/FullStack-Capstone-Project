140aaea71220145eb1d99aea106f935fb77ec1a1
AkshayWankhade23 <akshaywankhade664@gmail.com>
2023-12-12 18:23:57 +0530

    added database config and initialzation

--- server/index.js
+++ server/index.js
@@ -1,24 +1,27 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-dotenv.config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Middleware
-const app = express()
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        status: 'Success',
-        message: 'All Good!!'
-    })
-})
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "Success",
+    message: "All Good!!",
+    time: new Date(),
+  });
+});
 
 app.listen(process.env.PORT, () => {
-    mongoose
-        .connect(process.env.MONGODB_URL)
-        .then(() => console.log('server running on http://localhost:${process.env.PORT}'))
-        .catch(error => console.log(error))
-})
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() =>
+      console.log("server running on http://localhost:${process.env.PORT}")
+    )
+    .catch((error) => console.log(error));
+});
